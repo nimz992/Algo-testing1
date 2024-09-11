24a6a7cd5a7660a4c8610513e09cfd2a2d3c483b
nimz992 <nimrahanwar992@gmail.com>
2024-09-10 20:27:43 -0400

    Fixed Spec

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -14,16 +14,26 @@ describe("Data testing", () => {
       cy.title().should("eq", "Algo - Creative Intelligence");
       cy.url().should("include", "/home");
 
+      // Type 'Ask'
       cy.get("#mat-input-0", {
         timeout: 10000,
       }).type(`${value}{enter}`);
 
+      // Checks app-dynamic-tabs for response.
       cy.get(`[id="Panel - 00"] app-dynamic-tabs`, {
         timeout: 10000,
+      }).then(($body) => {
+        // Check if app-dynamic-message is displayed.
+        if ($body.find("app-dynamic-message").length > 0) {
+          // Check if app-dynamic-message has text "I am sorry".
+          cy.wrap($body)
+            .find("app-dynamic-message div div", { timeout: 1000 })
+            .should("not.contain", "I am sorry");
+        } else {
+          // Pass the test if the error message is not found
+          cy.log("No error message element found, test passed.");
+        }
       });
-
-      cy.get("app-dynamic-message").contains("I amadwad sorry");
-      cy.wait(100000);
     });
   });
 });
